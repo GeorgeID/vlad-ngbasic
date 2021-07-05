b04f09bf82d4150b1a491160cf33390a638f9e9a
GeorgeID <yu.webdev@yandex.ru>
2021-07-05 21:07:54 +0300

    binding method bugfix = замена logo Angular, button

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,3 +1,5 @@
+// TODO  binding method bugfix - замена logo Angular
+// import { Component, OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +7,8 @@ import { Component } from '@angular/core';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
 })
+// TODO  binding method bugfix = замена logo Angular, button
+// export class cardComponent implements OnInit {
 export class cardComponent {
   title = 'Мой заголовок карточки';
   text =
@@ -19,8 +23,20 @@ export class cardComponent {
     },
   };
   textObj = 'Так выглядит интерполяция obj|канал json';
+  disabled = false;
 
   getInfo(): string {
     return 'Это моя инфа - декоратор @Component () класс cardComponent( метод getInfo())';
   }
+
+  imgUrl: String = 'https://angular.io/assets/images/logos/angular/angular.svg';
+  // imgUrl2: String = 'https://www.dotcom-monitor.com/blog/wp-content/uploads/sites/3/2020/05/Vue-logo-1.png';
+
+  // TODO  binding method bugfix
+  // ngOnInit() {
+  //   setTimeout(handler: () => {
+  //     this.imgUrl = 'https://www.dotcom-monitor.com/blog/wp-content/uploads/sites/3/2020/05/Vue-logo-1.png'
+  //   this.disabled = true
+  //   }, timeout: 3000)
+  // }
 }
