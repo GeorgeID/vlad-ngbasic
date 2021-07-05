9e5b5c3aa0a113a212c5dbe5dc75d6884f43fda8
GeorgeID <yu.webdev@yandex.ru>
2021-07-05 19:26:00 +0300

    пример интерполяции переменнойб, метода класса cardComponent декоратора @Component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -5,4 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
 })
-export class cardComponent {}
+export class cardComponent {
+  title = 'Мой заголовок карточки';
+  text =
+    'Этот текст - пример интерполяции переменной text класса cardComponent';
+  // number = 42;
+  // array = [1, 1, 2, 3, 5, 8, 12];
+  obj = {
+    name: 'Владилен',
+    info: {
+      age: 25,
+      job: 'frontend',
+    },
+  };
+  textObj = 'Так выглядит интерполяция obj|канал json';
+
+  getInfo(): string {
+    return 'Это моя инфа - декоратор @Component () класс cardComponent( метод getInfo())';
+  }
+}
