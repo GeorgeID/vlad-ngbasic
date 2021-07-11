dfe8913a31b637d016f139dea11d1ad4d3344bc7
GeorgeID <yu.webdev@yandex.ru>
2021-07-11 21:13:11 +0300

    two-way binding  var 1/2

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -15,14 +15,14 @@ export class cardComponent {
     'Этот текст - пример интерполяции переменной text класса cardComponent';
   // number = 42;
   // array = [1, 1, 2, 3, 5, 8, 12];
-  obj = {
-    name: 'Владилен',
-    info: {
-      age: 25,
-      job: 'frontend',
-    },
-  };
-  textObj = 'Так выглядит интерполяция obj|канал json';
+  // obj = {
+  //   name: 'Владилен',
+  //   info: {
+  //     age: 25,
+  //     job: 'frontend',
+  //   },
+  // };
+  // textObj = 'Так выглядит интерполяция obj|канал json';
   disabled = false;
 
   getInfo(): string {
@@ -30,18 +30,28 @@ export class cardComponent {
   }
 
   imgUrl: String = 'https://angular.io/assets/images/logos/angular/angular.svg';
+  // imgUrl: String = '/src/assets/logo-angular.png';
   // imgUrl2: String = 'https://www.dotcom-monitor.com/blog/wp-content/uploads/sites/3/2020/05/Vue-logo-1.png';
 
   // TODO  binding method bugfix
   // ngOnInit() {
   //   setTimeout(handler: () => {
-  //     this.imgUrl = 'https://www.dotcom-monitor.com/blog/wp-content/uploads/sites/3/2020/05/Vue-logo-1.png'
-  //   this.disabled = true
+  //     this.imgUrl = 'https://www.dotcom-monitor.com/blog/wp-content/uploads/sites/3/2020/05/Vue-logo-1.png';
+  //   this.disabled = true;
   //   }, timeout: 3000)
   // }
 
   // Event binding
   changeTitle() {
-    this.title = 'Заголовок изменен'
+    this.title = 'Заголовок был изменен';
+  }
+  // // 1 - two-way binding
+  // inputHandler(event: any) {
+  //   const value = event.target.value;
+  //   this.title = value;
+  // }
+  // 1 - two-way binding
+  inputHandler(value: string) {
+    this.title = value;
   }
 }
