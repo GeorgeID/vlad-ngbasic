2b4b639e90f1b619639cefc25270e70adfd64265
GeorgeID <yu.webdev@yandex.ru>
2021-07-12 15:23:20 +0300

    Директивы упраления отображаемости элементов + структурные элементы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   // title = 'vlad-ngbasic';
+  
+  // Директивы упраления отображаемости элементов =
+  // Скрыть или показать карточки
+  // 1. Добавляем метод toggleCards
+  // 2. Создаем метод и переменную
+  toggle = true;
+  toggleCards() {
+    this.toggle = !this.toggle;
+  }
 }
