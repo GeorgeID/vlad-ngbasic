de0e1f355be8f4e3d29a7bd44d3cb6be8c2c1098
GeorgeID <yu.webdev@yandex.ru>
2021-07-14 13:34:51 +0300

    интернализация даты - bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+// интернализация даты - bug
+// import { registerLocaleData } from '@angular/common';
+// import localeRu from '@angular/common/locales/ru';
 
 import { AppComponent } from './app.component';
 import { cardComponent } from './card/card.component';
 import { FormComponent } from './form/form.component';
+
+// интернализация даты - bug
+// registerLocaleData (localeRu, localId:'ru')
 
 @NgModule({
   declarations: [AppComponent, cardComponent, FormComponent],
